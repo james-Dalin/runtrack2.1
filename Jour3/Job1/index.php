d7e9581f92b3b3e9d211140b22c54deeec0d3940
james.Dalin <james.dalin@laplateforme.io>
2025-09-19 14:03:35 +0200

    Job1

---
+++ Jour3/Job1/index.php
@@ -0,0 +1,11 @@
+<?php
+
+$nombre = array(200, 204, 173, 98, 171, 404, 459);
+
+echo $nombre[0] . " est paire <br/>";
+echo $nombre[1] . " est paire <br/>";
+echo $nombre[2] . " est impaire <br/>";
+echo $nombre[3] . " est paire <br/>";
+echo $nombre[4] . " est impaire <br/>";
+echo $nombre[5] . " est paire <br/>";
+echo $nombre[6] . " est impaire <br/>";
