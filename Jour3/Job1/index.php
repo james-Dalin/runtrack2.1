5ee91dc55e9e604cf7dec1469c1318fa5d8fbc6b
james.Dalin <james.dalin@laplateforme.io>
2025-09-23 16:38:48 +0200

    Job1 fait

--- Jour3/Job1/index.php
+++ Jour3/Job1/index.php
@@ -1,17 +1,21 @@
 <?php
 
 $nombres = [
-    "" => 200, 
-    "" => 204, 
-    "" => 173, 
-    "" => 98, 
-    "" => 171, 
-    "" => 404, 
-    "" => 459
+    200,
+    204,
+    173,
+    98,
+    171,
+    404,
+    459
 ];
 
-    foreach ($nombres as $nombre) {
-        if ($nombre)
+foreach ($nombres as $nombre) {
+    if ($nombre % 2 == 1) {
+        echo $nombre . " est impaire <br/>";
+    } elseif ($nombre % 2 == 0) {
+        echo $nombre . " est paire <br/>";
+    } else {
         echo $nombre . "<br/>";
     }
-
+}   
