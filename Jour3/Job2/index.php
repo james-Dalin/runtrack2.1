b220ef93e72ef46028aa4078e5f0ee5bc6f18d90
james.Dalin <james.dalin@laplateforme.io>
2025-09-24 13:49:32 +0200

    Fin du Job2. Début du cours Job3

--- Jour3/Job2/index.php
+++ Jour3/Job2/index.php
@@ -1,5 +1,6 @@
 <?php
 
-$str = ["Tous ces instants seront perdus dans le temps comme les larmes sous la pluie."];
-
-echo $str;
+$str = "Tous ces instants seront perdus dans le temps comme les larmes sous la pluie";
+$output = "";
+for($i = 0; $i < strlen($str); $i += 2) $output .= $str[$i];
+echo $output;
