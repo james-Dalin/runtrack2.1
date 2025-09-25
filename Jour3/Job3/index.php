b7015f6ca6f65abe11d8e35de13c2b0ee55e86ba
james.Dalin <james.dalin@laplateforme.io>
2025-09-25 13:56:54 +0200

    Fin du Job4

--- Jour3/Job3/index.php
+++ Jour3/Job3/index.php
@@ -1,8 +1,11 @@
 <?php
 
-$str = "I'm sorry Dave I'm afraid I can't do that";
+$strings = [ "I'm sorry Dave I'm afraid I can't do that"];
 $lonueur = strlen($str);
 
-for ($i = 0 ; $i < $longueur ; $i++) {
-    
+foreach ($strings as $string) {
+    for ($i = 0 ; $i < $longueur ; $i++) {
+        
+    }
 }
+
