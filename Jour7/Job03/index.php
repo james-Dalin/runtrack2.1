94cc101c7b2f8533ab472cb2f850b4db6826e631
drajoan258 <charlestedison@gmail.com>
2025-10-06 19:28:01 +0200

    Job03 fait

---
+++ Jour7/Job03/index.php
@@ -0,0 +1,8 @@
+<?php
+
+function getHello() {
+    return 'Hello LaPlateforme';
+}
+
+$bonjour = getHello(); //<== Avec cette variable, je récupère la valeur de "getHello"
+echo $bonjour; //<== J'affiche la variable.
