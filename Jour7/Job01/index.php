b334f093e08edeb4126619c96682ea236bb6dce5
drajoan258 <charlestedison@gmail.com>
2025-10-06 16:26:59 +0200

    Premier exercice fait

---
+++ Jour7/Job01/index.php
@@ -0,0 +1,11 @@
+<?php
+
+function hello() {
+  echo "Hello LaPlateforme";
+}
+hello();
+
+
+/* Je créais la fonction en lui donnant un nom explicite, puis je dis dans le bloque de code
+ce que j'ai que la fonction affiche, puis j'appelle la fonction pour l'afficher sur le
+navigateur. */
