5e2223b6cb114b5fe49652144c51fddb29587de7
drajoan258 <charlestedison@gmail.com>
2025-10-08 11:43:15 +0200

    Job06

---
+++ Jour7/Job06/index.php
@@ -0,0 +1,18 @@
+<?php
+
+function leetSpeak(string $str): string 
+{
+  $replacement = [
+    'A' => '4', 'a' => '4',
+    'B' => '8', 'b' => '8',
+    'E' => '3', 'e' => '3',
+    'G' => '6', 'g' => '6',
+    'L' => '1', 'l' => '1',
+    'S' => '5', 's' => '5',
+    'T' => '7', 't' => '7'
+  ];
+
+  return strtr($str, $replacement);
+}
+
+echo leetSpeak("Hello World");
