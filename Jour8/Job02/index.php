d1d6e9dacdb92653bbbe9a6b87c1609406fbd671
james.Dalin <james.dalin@laplateforme.io>
2025-10-13 15:53:17 +0200

    Job02 ajouté et fini

--- Jour8/Job02/index.php
+++ Jour8/Job02/index.php
@@ -1,2 +1,27 @@
 <?php
 
+if (isset($_POST['reset'])) {
+    setcookie('nbvisites', '', time() - 3600);
+    $nbvisites = 0;
+} else {
+    $nbvisites = isset($_COOKIE['nbvisites']) ? (int)$_COOKIE['nbvisites'] + 1 : 1;
+    setcookie('nbvisites', $nbvisites, time() + 3600);
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
+<p>Nombre de visites: <?php echo $nbvisites; ?></p>
+<form method="post">
+    <button name="reset" type="submit">Reset</button>
+</form>
